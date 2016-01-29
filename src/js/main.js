35d13cf095fcdd6ceb35d3d28ccefda9496126e1
jxl1983 <mike.gehrhardt@gmx.de>
2016-01-30 00:11:03 +0100

    progress-screen angelegt, noch nicht in ablauf eingebunden

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@ var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'ggj-2016', {});
 game.state.add('Boot', boot);
 game.state.add('Preloader', preloader);
 game.state.add('Menu', menu);
+game.state.add('Progress', progress);
 
 game.state.add('GameWon', gamewon);
 game.state.add('GameOver', gameover);
